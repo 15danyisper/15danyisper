c94213d99ad44ce45f6a6d5810583b4dbb4780c4
danyismail <dany_arie@yahoo.com>
2021-08-09 01:45:48 +0700

    solve no.3,4,5

--- main.go
+++ main.go
@@ -1,15 +1,6 @@
 package main
 
-import (
-	"fmt"
-)
-
-var listNumber = []int{4, 6, 3, 5, 4, 6, 7, 8, 3, 4, 6, 7, 5, 4, 6, 4, 4, 5, 6}
-
-type countMap struct {
-	num   int
-	count int
-}
+import "fmt"
 
 func swapVariable(a, b int) {
 	a, b = b, a
@@ -54,8 +45,99 @@ func howManyTimeAppears(n []int) {
 
 }
 
+func mergeTwoString(string1, string2 string) {
+	var result []string
+	for i := 0; i < len(string1) || i < len(string1); i++ {
+		if i < len(string1) {
+			result = append(result, string(string1[i]))
+		}
+
+		if i < len(string2) {
+			result = append(result, string(string2[i]))
+		}
+	}
+	var word string
+	for _, val := range result {
+		word += val
+	}
+	fmt.Println(word)
+}
+
+func jackDaniel(a string, b string) {
+	t := len(a) + len(b)
+	i, j := 0, 0
+	ret := make([]byte, t)
+	for i+j < t && i < len(a) && j < len(b) {
+		if b[j] < a[i] {
+			ret[i+j] = b[j]
+			j += 1
+			continue
+		}
+		ret[i+j] = a[i]
+		i += 1
+	}
+	ret = []byte(fmt.Sprintf("%s%s%s", ret, a[i:], b[j:]))
+	fmt.Println(string(ret))
+	//return string(ret)
+}
+
+func minMax(array []int) (int, int) {
+	var max int = array[0]
+	var min int = array[0]
+	for _, value := range array {
+		if max < value {
+			max = value
+		}
+		if min > value {
+			min = value
+		}
+	}
+	return min, max
+}
+
+func mOperation(input [][]int) {
+	initial := []int{0, 0, 0, 0, 0}
+	for i := 0; i <= len(input)-1; i++ {
+		for j := input[i][0] - 1; j <= input[i][1]-1; j++ {
+			initial[j] += input[i][2]
+		}
+	}
+	fmt.Println(initial)
+	_, max := minMax(initial)
+	fmt.Println(max)
+}
+
 func main() {
-	fmt.Println("Hello world...")
+	fmt.Println(".:Start:.")
+	fmt.Print("\n")
+
+	fmt.Println("No.1")
 	swapVariable(3, 5)
-	howManyTimeAppears(listNumber)
+	fmt.Print("\n")
+
+	fmt.Println("No.2")
+	howManyTimeAppears([]int{4, 6, 3, 5, 4, 6, 7, 8, 3, 4, 6, 7, 5, 4, 6, 4, 4, 5, 6})
+	fmt.Print("\n")
+
+	fmt.Println("No.3")
+	mergeTwoString("saya", "kamu")
+	mergeTwoString("kosong", "ada")
+	mergeTwoString("ada", "kosong")
+	fmt.Print("\n")
+
+	fmt.Println("No.4")
+	jackDaniel("JACK", "DANIEL")
+	jackDaniel("ABACABA", "ABACABA")
+	fmt.Print("\n")
+
+	fmt.Println("No.5")
+	var inputListNumber = [][]int{
+		{1, 2, 100},
+		{2, 5, 100},
+		{3, 4, 100},
+	}
+	mOperation(inputListNumber)
+
+	fmt.Print("\n")
+	fmt.Println(".:Finished:.")
 }
